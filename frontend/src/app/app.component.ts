c385f71bc97f18157c8b3529356cdaac1dc3b003
Samer Alkhatib <conan.500@hotmail.com>
2024-11-16 08:49:41 +0200

    Update app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import {
+  BehaviorSubject,
+  combineLatest,
+  filter,
+  map,
+  startWith,
+  switchMap,
+} from 'rxjs';
+import { environment } from '../environments/environment';
+
+type User = { id?: number; name: string };
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'frontend';
+  private readonly http = inject(HttpClient);
+
+  addButtonClicked$ = new BehaviorSubject<string | null>(null);
+
+  addUser$ = this.addButtonClicked$.pipe(
+    filter((name) => !!name),
+    map((name) => name as string),
+    switchMap((name) => this.http.post(environment.apiUrl + 'user', { name }))
+  );
+
+  users$ = combineLatest({ addUser: this.addUser$ }).pipe(
+    startWith({ addUser: null }),
+    switchMap(() => {
+      return this.http.get<User[]>(environment.apiUrl + 'user');
+    })
+  );
+
+  users = toSignal(this.users$);
+
+  onAddUser(name: HTMLInputElement) {
+    this.addButtonClicked$.next(name.value);
+    name.value = '';
+    name.focus();
+  }
 }
